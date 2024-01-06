23247e2a58f645c2971aef9a0517473caa9a5f69
Cahid <mr.cebizade@gmail.com>
2024-01-07 03:15:12 +0400

    Project folder structere init

---
+++ include/main.h
@@ -0,0 +1,9 @@
+//
+// Created by mrceb on 1/7/2024.
+//
+
+#ifndef DSQL_MAIN_H
+#define DSQL_MAIN_H
+#include <stdio.h>
+void print(char *);
+#endif //DSQL_MAIN_H
---
+++ src/print.c
@@ -0,0 +1,9 @@
+//
+// Created by mrceb on 1/7/2024.
+//
+#include "main.h"
+
+void print(char *text)
+{
+    printf("%s\n", text);
+}
